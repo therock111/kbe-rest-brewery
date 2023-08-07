5af3e1dced628a53ed51530bdc329df78ee42e21
therock111 <coc10214@gmail.com>
2023-08-07 22:26:05 +0530

    Dockerfile is configured and is working

--- src/main/java/guru/springframework/sfgrestbrewery/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/sfgrestbrewery/services/CustomerServiceImpl.java
@@ -6,9 +6,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
-/**
- * Created by jt on 2019-04-21.
- */
+
 @Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService {
